7fcc36755a625c2042215837f5ea71d753bb32a4
CatoCoin <iamcatocoin@gmail.com>
2018-06-23 18:32:38 -0400

    updated

--- catoinstall.sh
+++ catoinstall.sh
@@ -44,7 +44,7 @@ function download_node() {
   echo -e "${GREEN}Downloading and Installing VPS $COIN_NAME Daemon${NC}"
   cd $TMP_FOLDER >/dev/null 2>&1
   wget -q $COIN_TGZ
-  tar xzvf smn4.tar.gz
+  tar xzvf catocoin-ubuntu-16-04-v1.tar.gz
   cd smn4 >/dev/null 2>&1
   chmod +x $COIN_DAEMON $COIN_CLI
   cp $COIN_DAEMON $COIN_CLI $COIN_PATH
