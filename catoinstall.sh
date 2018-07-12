59deb147c26f9c61a258e0511628d48b27c6392c
CatoCoin <40036779+CatoCoin@users.noreply.github.com>
2018-07-11 23:04:50 -0400

    Update catoinstall.sh
    update

--- catoinstall.sh
+++ catoinstall.sh
@@ -77,8 +77,8 @@ EOF
 
   systemctl daemon-reload
   sleep 3
-  #systemctl start $COIN_NAME.service
-  systemctl enable $COIN_NAME.service >/dev/null 2>&1
+  systemctl start $COIN_NAME.service
+  #systemctl enable $COIN_NAME.service >/dev/null 2>&1
 
   if [[ -z "$(ps axo cmd:100 | egrep $COIN_DAEMON)" ]]; then
     echo -e "${RED}$COIN_NAME is not running${NC}, please investigate. You should start by running the following commands as root:"
