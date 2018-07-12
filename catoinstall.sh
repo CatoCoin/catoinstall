59f7bfc1cc62c9dd07a50125c8846b3fd30834a3
CatoCoin <40036779+CatoCoin@users.noreply.github.com>
2018-07-11 22:28:29 -0400

    Update catoinstall.sh
    updates

--- catoinstall.sh
+++ catoinstall.sh
@@ -235,7 +235,7 @@ clear
 function important_information() {
  echo
  echo -e "${BLUE}================================================================================================================================${NC}"
- echo -e "${PURPLE}Windows Wallet Guide. https://github.com/Realbityoda/catocoin/blob/master/README.md${NC}"
+ echo -e "${PURPLE}Windows Wallet Guide. COMING SOON${NC}"
  echo -e "${BLUE}================================================================================================================================${NC}"
  echo -e "$COIN_NAME Masternode is up and running listening on port ${GREEN}$COIN_PORT${NC}."
  echo -e "Configuration file is: ${RED}$CONFIGFOLDER/$CONFIG_FILE${NC}"
